4b21df99e381d441ed14bf114e4519d933533c71
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-01-24 20:37:30 +0300

    django: static & media

--- project/config/settings.py
+++ project/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,8 +116,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_URL = "static/"
+# URL для медии и статики в шаблонах
+MEDIA_URL = "/media/"
+STATIC_URL = "/static/"
+
+# пустая папка, сюда будет собирать статику collectstatic
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
+
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "assets"),)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
