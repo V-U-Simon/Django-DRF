048c719a60daab958d4a874ace7a6461b4a2d8c4
Willy Wonka <Woodenehad9798740@yandex.ru>
2023-01-24 23:13:40 +0300

    django: debug-toolbar

--- project/config/urls.py
+++ project/config/urls.py
@@ -5,9 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("todo/", include("app_todo.urls")),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
